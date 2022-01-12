bf8573ed7266606cb1def2c49240254c0422d0d9
brianartea <bri@expertdesign.cc>
2022-01-11 20:35:37 -0500

    bg-gray-100

--- frontend_sanity/src/components/PinDetail.jsx
+++ frontend_sanity/src/components/PinDetail.jsx
@@ -127,7 +127,7 @@ const PinDetail = ({ user }) => {
                 className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none"
                 onClick={addComment}
               >
-                {addingComment ? 'Doing...' : 'Done'}
+                {addingComment ? 'Adding Comment...' : 'Add Comment'}
               </button>
             </div>
           </div>
@@ -141,7 +141,7 @@ const PinDetail = ({ user }) => {
       {pins ? (
         <MasonryLayout pins={pins} />
       ) : (
-        <Spinner message="Loading more pins" />
+        <Spinner message="Loading more pins..." />
       )}
     </>
   );
